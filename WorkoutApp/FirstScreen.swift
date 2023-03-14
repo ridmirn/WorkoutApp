1be764715c92043bdfcb91f9656a09a7be2c7334
ridmirn <ridmi.natasha.rn@gmail.com>
2023-03-14 17:31:50 +0530

    First screen with button

--- WorkoutApp/FirstScreen.swift
+++ WorkoutApp/FirstScreen.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class FirstScreen: UIViewController {
     
+    let StartButton = UIButton()
+    
     let ImageView: UIImageView = {
         let NewImageView = UIImageView()
         NewImageView.image = UIImage(named:"AppLogo")
@@ -21,7 +23,9 @@ class FirstScreen: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = .black
         view.addSubview(ImageView)
+        view.addSubview(StartButton)
         ImageViewConstraints()
+        setStartButton()
         
     }
     
@@ -29,7 +33,26 @@ class FirstScreen: UIViewController {
         ImageView.widthAnchor.constraint(equalToConstant: 180).isActive = true
         ImageView.heightAnchor.constraint(equalToConstant: 180).isActive = true
         ImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        ImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 10).isActive = true
+        ImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -80).isActive = true
+    }
+    
+    func setStartButton(){
+        
+        StartButton.configuration = .filled()
+        StartButton.configuration?.baseBackgroundColor = .systemGreen
+        StartButton.configuration?.title = "Get Started"
+    
+        
+        StartButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            StartButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 120),
+            StartButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            StartButton.widthAnchor.constraint(equalToConstant: 250),
+            StartButton.heightAnchor.constraint(equalToConstant: 70)
+        ])
+        
+        
     }
 
 
